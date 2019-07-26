e710e502cd28761a8e4109dd9de16ddf43843d53
moraloyuxing <s110319023@stu.ntue.edu.tw>
2019-07-26 17:45:00 +0800

    0726_巡邏&箱子

---
+++ ForceUpload/Assets/Scripts/Lin/Box_Move.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box_Move : MonoBehaviour{
+    public Vector3 Ini_Start;
+    public Vector3 Ini_End;
+    float speed = 0.4f;
+    float FirstSpeed;
+    public TileMap _tilemap;
+    bool NextPosisWalkable = false;
+
+    void Start(){
+        FirstSpeed = Vector3.Distance(Ini_Start, Ini_End) * speed;
+        speed = 0;
+    }
+
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.A)) {
+            Ini_Start = transform.position;
+            Ini_End = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
+            NextPosisWalkable = _tilemap.CheckTileWalkable(Ini_End.x,Ini_End.z);
+            if (NextPosisWalkable == true) {
+                _tilemap.SetTileOccupy(Ini_Start.x, Ini_Start.z, false);
+                _tilemap.SetTileOccupy(Ini_End.x,Ini_End.z,true);
+                speed = CalculateNewSpeed();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)){
+            Ini_Start = transform.position;
+            Ini_End = new Vector3(transform.position.x, transform.position.y, transform.position.z-1);
+            NextPosisWalkable = _tilemap.CheckTileWalkable(Ini_End.x, Ini_End.z);
+            if (NextPosisWalkable == true){
+                _tilemap.SetTileOccupy(Ini_Start.x, Ini_Start.z, false);
+                _tilemap.SetTileOccupy(Ini_End.x, Ini_End.z, true);
+                speed = CalculateNewSpeed();
+            }
+        }
+
+        if (speed != 0) {
+            transform.position = Vector3.Lerp(transform.position, Ini_End, speed * Time.deltaTime);
+            speed = CalculateNewSpeed();
+        }
+    }
+
+    float CalculateNewSpeed(){
+        float tmp_dis = Vector3.Distance(transform.position, Ini_End);
+
+        if (tmp_dis == 0){
+            return tmp_dis;
+        }
+        else return (FirstSpeed / tmp_dis);
+    }
+
+}
---
+++ ForceUpload/Assets/Scripts/Lin/PatrolRobot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolRobot : MonoBehaviour{
+
+    public Vector3 Ini_Start;
+    public Vector3 Ini_End;
+    Vector3 tmp_switch;
+
+    float speed = 0.4f;
+    float FirstSpeed;
+
+    void Start(){
+        FirstSpeed = Vector3.Distance(Ini_Start, Ini_End) * speed;
+    }
+
+    void Update(){
+        transform.position = Vector3.Lerp(transform.position, Ini_End, speed * Time.deltaTime);
+        speed = CalculateNewSpeed();
+    }
+
+    float CalculateNewSpeed() {
+        float tmp_dis = Vector3.Distance(transform.position, Ini_End);
+
+        if (tmp_dis == 0){
+            tmp_switch = Ini_Start;
+            Ini_Start = Ini_End;
+            Ini_End = tmp_switch;
+            tmp_dis = Vector3.Distance(transform.position, Ini_End);
+        }
+        return (FirstSpeed / tmp_dis);
+    }
+
+}
